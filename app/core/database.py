d1c46ecf3465c3d9507b367cdcdf1b15105be219
Veera Manikanta <veeramanikanta@Veeras-MacBook-Pro.local>
2026-05-25 14:53:25 -0400

    remove db url

--- app/core/database.py
+++ app/core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://neondb_owner:npg_F4lo0aYMHGsg@ep-odd-paper-aq72d3i4-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+DATABASE_URL = "your_database_url_here"
 
 engine = create_engine(DATABASE_URL)
 
